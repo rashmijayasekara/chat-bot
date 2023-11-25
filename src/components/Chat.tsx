5cd243c99274b54faf12d465288cac97100354ea
Rashmi <rashmijayasekara00@gmail.com>
2023-11-25 14:15:44 +0530

    Creating collapsed chat

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,5 +1,6 @@
-import { Accordion, AccordionItem, AccordionTrigger } from "@radix-ui/react-accordion";
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@radix-ui/react-accordion";
 import {FC} from 'react';
+import ChatHeader from "./ChatHeader";
 
 const Chat: FC = () => {
     return (
@@ -7,7 +8,15 @@ const Chat: FC = () => {
             <AccordionItem value="item-1">
                 <div className="fixed right-8 w-80 bottom-8 bg-white border border-gray-200 rounded-md overflow-hidden">
                     <div className="w-full h-full flex flex-col">
-                        <AccordionTrigger className="px-6 border-b border-zinc-300">Hellow</AccordionTrigger>
+                        <AccordionTrigger className="px-6 border-b border-zinc-300">
+                            <ChatHeader/>
+                        </AccordionTrigger>
+                        <AccordionContent>
+                            <div className="flex flex-col h-80">
+                                messages
+                                <ChatInput />
+                            </div>
+                        </AccordionContent>
                     </div>
                 </div>
             </AccordionItem>
